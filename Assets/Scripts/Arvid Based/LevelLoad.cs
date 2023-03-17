236d9707796f274dc2b8ea0d677eb28d95cbd479
ArKaLbs <arvid.karlsson@elev.ga.lbs.se>
2023-03-17 12:29:56 +0100

    En kod

--- Assets/Scripts/Arvid Based/LevelLoad.cs
+++ Assets/Scripts/Arvid Based/LevelLoad.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class LevelLoad : MonoBehaviour
 {
 
+    public Transform player;
+    public float smoothspeed;
+
+    private Vector3 targetspot, newspot;
+
+    public Vector3 minspot, maxspot;
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (transform.position != player.position)
         {
-            LoadNextScene();
+            Vector3 camBoundaryPos = new Vector3(
+                Mathf.Clamp(targetspot.x, minspot.x, maxspot.x),
+                Mathf.Clamp(targetspot.y, minspot.y, maxspot.y),
+                Mathf.Clamp(targetspot.z, minspot.z, maxspot.z));
+
+            newspot = Vector3.Lerp(transform.position, camBoundaryPos, smoothspeed);
+            transform.position = newspot;
         }
     }
-    public void LoadNextScene()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
+
 }
