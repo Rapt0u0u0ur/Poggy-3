4d9890f12e20830cd65c0d3490a3c6dfbf33e7d2
Noah <noah.dove@elev.ga.lbs.se>
2023-03-20 14:37:00 +0100

    forg sprint

--- Assets/Scripts/The brit/Basic_motor_function.cs
+++ Assets/Scripts/The brit/Basic_motor_function.cs
@@ -6,6 +6,8 @@ public class Basic_motor_function : MonoBehaviour
 {
     Vector3 Goal_2;
     Vector3 Goal;
+    Vector2 SprintGoal;
+    bool Sprinting;
     public float speed;
     // Start is called before the first frame update
     void Start()
@@ -16,42 +18,45 @@ public class Basic_motor_function : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.position = Vector2.MoveTowards(transform.position, Goal, speed * Time.deltaTime);
         if (transform.position == Goal)
-        {
             Goal = Goal_2;
-        }
-        transform.position = Vector3.MoveTowards(transform.position, Goal, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.W))
+            ActionDecider(1, 0);
+        else if (Input.GetKeyDown(KeyCode.S))
+            ActionDecider(-1, 0);
+        else if (Input.GetKeyDown(KeyCode.A))
+            ActionDecider(0, -1);
+        else if (Input.GetKeyDown(KeyCode.D))
+            ActionDecider(0, 1);
+        if (Sprinting == true)
+            ActionDecider((int)SprintGoal.y, (int)SprintGoal.x);
+        
         if (Input.GetKey(KeyCode.LeftShift))
         {
-
+            Sprinting = true;
         }
-        if (Input.GetKeyDown(KeyCode.W))
+        else
         {
-            ActionDecider(1, 0);
-        }
-        else if (Input.GetKeyDown(KeyCode.S))
-        {
-            ActionDecider(-1, 0);
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            ActionDecider(0, -1);
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            ActionDecider(0, 1);
+            Sprinting = false;
+            SprintGoal = new Vector2();
         }
     }
     void ActionDecider(int y, int x)
     {
         if (transform.position != Goal)
         {
-            Goal_2 = Goal + new Vector3(x, y, 0);
+            Goal_2 = Goal + new Vector3(x, y);
         }
         else
         {
-            Goal += new Vector3(x, y, 0);
+            Goal += new Vector3(x, y);
             Goal_2 = Goal;
         }
+
+        if (Sprinting == true)
+            SprintGoal = new Vector2(x, y);
+
     }
 }
